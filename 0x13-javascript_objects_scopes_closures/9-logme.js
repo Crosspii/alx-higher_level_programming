97a1a3d70d22d9b7160814dfa8c13b5f9d2f00a7
Aymen <crosspieboo@gmail.com>
2024-12-21 20:48:24 -0800

    fixing error in file 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -3,6 +3,6 @@
 let numberOfArguments = 0;
 
 exports.logMe = function (item) {
-  console.log('${numberOfArguments}: ${item}');
+  console.log(`${numberOfArguments}: ${item}`);
   numberOfArguments++;
 };
