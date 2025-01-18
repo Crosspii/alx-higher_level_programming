5ca47203b00ceb74e2884257533abe16cdcbc8ae
Aymen Hm <crosspieloo@gmail.com>
2025-01-17 20:46:41 -0800

    fixing task 6 email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Sends a POST request to the passed URL with the email as a parameter."""
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    email = {'email': sys.argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
